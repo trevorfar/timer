4b689fba9b1919627b0b05ae9ddd4f6d8bdf370d
Trevor Farias <levelonethousandr@gmail.com>
2025-03-22 15:20:26 -0400

    push

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     reactStrictMode: true,
-    distDir: 'build',
-  }
+    swcMinify: true,
+  };
+  
+  module.exports = nextConfig;
   
